064bd52f0595c67dbed0c03f57219567d1c9a7c3
Juan <juancruzardanaz@gmail.com>
2022-11-04 00:15:10 -0300

    navbar display menu adjustment

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,73 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
 import Logo from '../../assets/img/logo.svg';
-import Hamburger from '../../assets/img/icon-hamburger.svg';
 import '../../scss/components/_navbar.scss';
 import {Link} from 'react-scroll';
 
 /*==================== NAVBAR ====================*/
-const Navbar = () => {
-  return (
-    <header className='header' id='header'>
-        <nav className='nav container' id='nav_menu'>
-            <a href="#">
-                <img src={Logo} alt='logo' className='nav-logo'/>
-            </a>
+function Navbar() {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
 
-            <div className="nav_menu" id="nav-menu">
-                <ul className="nav_list">
-                    <li className="nav_item">
-                        <Link to="hero" spy={true} smooth={true} offset={-30} duration={500} className="nav_link">Home</Link>
-                    </li>
-                    <li className="nav_item">
-                        <Link to="choose" spy={true} smooth={true} offset={-30} duration={500} className="nav_link">About</Link>
-                    </li>
-                    <li className="nav_item">
-                        <Link to="footer" spy={true} smooth={true} offset={50} duration={500} className="nav_link">Contact</Link>
-                    </li>
-                    <li className="nav_item">
-                        <Link to="articles" spy={true} smooth={true} offset={-30} duration={500} className="nav_link">Blog</Link>
-                    </li>
-                    <li className="nav_item">
-                        <Link to="footer" spy={true} smooth={true} offset={50} duration={500} className="nav_link">Careers</Link>
-                    </li>
-                </ul>
-            </div>
+    return (
+        <header className='header' id='header'>
+            <nav className='nav container'>
+                <a href="#">
+                    <img src={Logo} alt='logo' className='nav-logo'/>
+                </a>
 
-            <div className='nav_toggle' id='nav-toggle'>
-                <img src={Hamburger} alt='hamburger' className='nav_hamburger'/>
-            </div>
+                {/* Desktop Menu */}
+                <div className="nav_menu" id="nav-menu">
+                    <ul className="nav_list">
+                        <li className="nav_item">
+                            <a href="#hero" className="nav_link">Home</a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#choose" className="nav_link">About</a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#footer" className="nav_link">Contact</a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#articles" className="nav_link">Blog</a>
+                        </li>
+                        <li className="nav_item">
+                            <a href="#footer" className="nav_link">Careers</a>
+                        </li>
+                    </ul>
+                </div>
 
-            <Link to="header" className='button button-header' spy={true} smooth={true} offset={50} duration={500}>
-                Request Invite
-            </Link>
-        </nav>
-    </header>
-  )
+                {/* Hamburger */}
+                <div onClick={handleClick} className='nav-hamburger'>
+                    {!nav ? <FaBars /> : <FaTimes />}
+                </div>
+
+                {/* Mobile Menu */}
+                <div className={!nav ? 'hidden' : 'nav_mobile_menu'}>
+                    <ul className='nav_mobile_container'>
+                        <li className="nav_mobile_item">
+                            <a href="#hero" className="nav_mobile_link">Home</a>
+                        </li>
+                        <li className="nav_mobile_item">
+                            <a href="#choose" className="nav_mobile_link">About</a>
+                        </li>
+                        <li className="nav_mobile_item">
+                            <a href="#footer" className="nav_mobile_link">Contact</a>
+                        </li>
+                        <li className="nav_mobile_item">
+                            <a href="#articles" className="nav_mobile_link">Blog</a>
+                        </li>
+                        <li className="nav_mobile_item">
+                            <a href="#footer" className="nav_mobile_link">Careers</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <Link to="header" className='button button-header' spy={true} smooth={true} offset={50} duration={500}>
+                    Request Invite
+                </Link>
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar;
 
-/* ========= SHOW MENU ========= */
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
-    
-    // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', () => {
-            // We add the show-menu class to the div tag with the nav_menu class
-            nav.classList.toggle('show-menu');
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu');
-
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav_link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav_link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction));
